b1b5e700dc0d7921dd19f3b5e2972f13e5c68f04
Kevin.D <49967540+kwwzzz@users.noreply.github.com>
2020-03-25 19:25:27 +0800

    Update the pic and string for ubuntukylin 20.04 (#1)

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="ubuntu-kylin-docs"
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -d $srcdir/ubuntu-kylin-help) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
